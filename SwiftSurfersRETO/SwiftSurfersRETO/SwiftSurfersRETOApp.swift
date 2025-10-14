ff29297f9755400606d84c6d6b66a857e3f9475f
Salvador-Ancer <141672061+Salvador-Ancer@users.noreply.github.com>
2025-10-14 11:15:53 -0600

    Proyectyo base

---
+++ SwiftSurfersRETO/SwiftSurfersRETO/SwiftSurfersRETOApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSurfersRETOApp.swift
+//  SwiftSurfersRETO
+//
+//  Created by Salvador Ancer on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSurfersRETOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
