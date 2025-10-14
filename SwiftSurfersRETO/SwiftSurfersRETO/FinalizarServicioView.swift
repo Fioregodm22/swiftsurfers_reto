2ec8517c8fdcf7dba183b65bb7f83d8cdbbd2bd1
Sofía Reyes <A00839262@tec.mx>
2025-10-14 16:27:26 -0600

    cambios en FinalizarServicioView

--- SwiftSurfersRETO/SwiftSurfersRETO/FinalizarServicioView.swift
+++ SwiftSurfersRETO/SwiftSurfersRETO/FinalizarServicioView.swift
@@ -11,6 +11,12 @@ struct FinalizarServicioView: View {
     
     @State public var kmFinal: Double? = nil
     @State public var distanciaRecorrida: Double? = nil
+    @State private var errorKMFinal : Bool = false
+    
+    @State var calendario = Calendar.current
+    @State var horaInicio = Date()
+    
+    @State var kmPrueba: Int = 1800
     
     
     let gris1 = Color(red: 242/255.0, green: 242/255.0, blue: 242/255.0)
@@ -22,25 +28,40 @@ struct FinalizarServicioView: View {
     let blancoClaro = Color(red: 251/255.0, green: 251/255.0, blue: 251/255.0)
     
     var body: some View {
+        
+        let hora = calendario.component(.hour, from: horaInicio)
+        
+        let minuto = calendario.component(.minute, from: horaInicio)
+        
         VStack{
             //PARTE SUPERIOR
             ZStack(alignment: .topLeading){
                 Color(azul)
                     .ignoresSafeArea(edges: .top)
                 //RESUMEN STACK
-                Text("Finalizar Servicio")
-                    .padding(.top, 20)
-                    .padding(.leading, 60)
-                    .foregroundStyle(Color.white)
-                    .bold()
-                    .font(.system(size: 30))
-                Text ("# ID: 001")
-                    .padding(.top, 70)
-                    .padding(.leading, 60)
-                    .foregroundStyle(Color.white)
-                    .font(.system(size: 20))
+                HStack (alignment: .center, spacing: 16) {
+                    Image("novaLogo1")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:80)
+                        .padding(.leading, 20)
+                        .padding(.top, 10)
+                    
+                    VStack (alignment: .leading, spacing: 8) {
+                        Text("Finalizar Servicio")
+                            .padding(.top, 25)
+                            .padding(.leading, 5)
+                            .foregroundStyle(Color.white)
+                            .bold()
+                            .font(.system(size: 25))
+                        Text ("# ID: 001")
+                            .padding(.leading, 5)
+                            .foregroundStyle(Color.white)
+                            .font(.system(size: 20))
+                    }
+                }
+                .padding(.top, 5)
             }
-            
             .frame(maxWidth: .infinity)
             .frame(height: 120)
             .padding(.bottom, 40)
@@ -50,59 +71,70 @@ struct FinalizarServicioView: View {
                 ZStack(alignment: .top) {
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color(blancoClaro))
-                        .frame(width: 350, height: 180)
+                        .frame(width: 350, height: 160)
 
                     VStack(alignment: .leading, spacing: 16) {
                         Text("Hora de inicio:")
-                            .padding(.bottom, 10)
+                            .padding(.bottom, 7)
+                            .foregroundStyle(gris4)
 
-                        Text("Hora de finalización:")
-                            .padding(.bottom, 8)
-                        
-                        Divider().frame(width: 320)
+                        HStack(alignment: .firstTextBaseline) {
+                                Text("Hora de finalización:")
+                                    .foregroundStyle(gris4)
+
+                                Spacer()
+
+                                Text(String(format: "%02d:%02d", hora, minuto))
+                                        .font(.system(size: 15, weight: .bold))
+                                        .foregroundStyle(gris4)
+                                    }
+
+                                Divider().frame(width: 320)
+
                         Text("Tiempo total: ")
-                            .padding(.bottom, 8)
-                        
-                    }
-                    .padding(.top, 38)
-                    .padding(.leading, 30)
-                    .padding(.trailing, 20)
+                                        .foregroundStyle(gris4)
+                        }
+                            .padding(.top, 28)
+                            .padding(.horizontal, 30)
 
-                    Text("RESUMEN")
-                        .font(.system(size: 20))
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 6)
-                        .background(RoundedRectangle(cornerRadius: 20).fill(Color(naranja)))
-                        .foregroundStyle(.white)
-                        .offset(y: -18)
-                }
-                .padding(.top, 2)
-            }
+                        Text("RESUMEN")
+                            .font(.system(size: 20))
+                            .padding(.horizontal, 40)
+                            .padding(.vertical, 6)
+                            .background(RoundedRectangle(cornerRadius: 20).fill(Color(naranja)))
+                                    .foregroundStyle(.white)
+                                    .offset(y: -18)
+                        }
+                        .padding(.top, 1)
+                    }
 
             //PARTE KILOMETRAJE
             VStack {
                 ZStack(alignment: .top) {
                     RoundedRectangle(cornerRadius: 20)
                         .fill(Color(blancoClaro))
-                        .frame(width: 350, height: 280)
+                        .frame(width: 350, height: 255)
 
                     VStack(alignment: .leading, spacing: 16) {
                         Text("KM Inicial:")
                             .padding(.leading, 30)
+                            .foregroundStyle(gris4)
 
                         VStack(alignment: .leading, spacing: 8) {
                             Text("KM Final*")
                                 .padding(.leading, 30)
                                 .padding(.top, 20)
                                 .padding(.bottom, 10)
+                                .foregroundStyle(gris4)
 
                             HStack {
                                 Spacer()
                                 TextField("",value: $kmFinal, format: .number)
+                                    .padding(.horizontal, 16)
                                     .frame(width: 230, height: 40)
                                     .background(RoundedRectangle(cornerRadius: 20).fill(Color(gris1)))
+                                    .foregroundStyle(gris4)
                                 Spacer()
-                                
                             }
                             .padding(.top, 10)
                             
@@ -130,28 +162,44 @@ struct FinalizarServicioView: View {
 
                     Text("KILOMETRAJE")
                         .font(.system(size: 20))
-                        .padding(.horizontal, 12)
+                        .padding(.horizontal, 40)
                         .padding(.vertical, 6)
                         .background(RoundedRectangle(cornerRadius: 20).fill(Color(naranja)))
                         .foregroundStyle(.white)
                         .offset(y: -18)
                 }
-                .padding(.top, 30)
+                .padding(.top, 25)
             }
             //BOTONES
             VStack {
-                Button("FINALIZAR") {}
-                    .buttonStyle(.borderedProminent)
-                    .padding(.top, 10)
-                    .tint(azul)
-                    .bold()
+                Button("FINALIZAR") {
+                    if (kmFinal == nil){
+                        errorKMFinal.toggle( )
+                    }
+                    
+                }
+                .padding(.top, 20)
+                .padding(.bottom, 20)
+                .padding(.horizontal, 110)
+                .foregroundStyle(.white)
+                .background(RoundedRectangle(cornerRadius: 20).fill(Color(azul)))
+                .bold(true)
+                    .alert("Error", isPresented: $errorKMFinal){
+                        Button("Aceptar"){}
+                    }message: {
+                        Text("Debes de ingresar un kilometraje válido")
+                    }
+                   
                 
                 Button("CANCELAR") {}
-                    .buttonStyle(.borderedProminent)
-                    .padding(.top, 10)
-                    .tint(gris4)
-                    .bold()
+                    .padding(.top, 20)
+                    .padding(.bottom, 20)
+                    .padding(.horizontal, 110)
+                    .foregroundStyle(.white)
+                    .background(RoundedRectangle(cornerRadius: 20).fill(Color(gris4)))
+                    .bold(true)
             }
+            //.padding(.top, 5)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .background(gris2.opacity(0.3))
