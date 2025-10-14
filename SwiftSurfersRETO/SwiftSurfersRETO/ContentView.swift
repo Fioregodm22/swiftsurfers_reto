0ba36bbd25f04ce3038e67c4b2caa034e43b4aa3
mariacavada <a01723282@tec.mx>
2025-10-14 12:39:28 -0600

    test-2

--- SwiftSurfersRETO/SwiftSurfersRETO/ContentView.swift
+++ SwiftSurfersRETO/SwiftSurfersRETO/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("test")
+            Text("hola-2")
         }
         .padding()
     }
