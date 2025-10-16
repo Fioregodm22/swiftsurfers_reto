0fb2c14bcdb4bd6abcf9bf780b5ce511c34e0804
Alejandra Saldaña <A00838889@tec.mx>
2025-10-15 18:56:15 -0600

    Navegacion

--- SwiftSurfersRETO/SwiftSurfersRETO/DetalleView.swift
+++ SwiftSurfersRETO/SwiftSurfersRETO/DetalleView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct DetalleView: View {
+    @State private var navegarAIniciar = false
+    let naranja = Color(red: 255/255.0, green: 153/255.0, blue: 0/255.0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        Text("DETALLE VIEW")
+        Text("SE VA A DESARROLLAR EN LOS SIGUIENTES SPRINTS")
+        Spacer()
+        
+        Button(action: {
+            navegarAIniciar = true
+        }) {
+            HStack(spacing: 12) {
+                Image(systemName: "checkmark.circle.fill")
+                    .font(.system(size: 20))
+                
+                Text("Iniciar Servicio")
+                    .font(.system(size: 20))
+                    .bold(true)
+            }
+            .frame(width: 300)
+            .frame(height: 60)
+            .foregroundColor(.white)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(naranja)
+            )
+        }
+        .navigationDestination(isPresented: $navegarAIniciar) {
+            IniciarServicioView()
+        }
+        .padding(.horizontal)
+        .toolbar(.hidden)
+        .navigationBarTitle("Volver")
+    }}
+    
 
 #Preview {
-    DetalleView()
+    NavigationStack {
+        DetalleView()
+    }
 }
