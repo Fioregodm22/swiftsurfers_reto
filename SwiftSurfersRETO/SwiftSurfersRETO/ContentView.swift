b65f5618f1e350fe29368566bf833d205a60f244
Salvador-Ancer <A00840038@tec.mx>
2025-11-12 18:45:53 -0600

    navigation

--- SwiftSurfersRETO/SwiftSurfersRETO/ContentView.swift
+++ SwiftSurfersRETO/SwiftSurfersRETO/ContentView.swift
@@ -12,16 +12,18 @@ struct ContentView: View {
     @State private var tabSelection = 2
     
     var body: some View {
-        TabView(selection: $tabSelection) {
-            Text("Tab Content 2").tag(1)
-            
-            AgendaView().tag(2)
-            
-            Text("Tab Content 3").tag(3)
-
-        }
-        .overlay(alignment: .bottom){
-            CustomTabView(tabSelection: $tabSelection)
+        NavigationStack{
+            TabView(selection: $tabSelection) {
+                Text("Tab Content 2").tag(1)
+                
+                AgendaView().tag(2)
+                
+                Text("Tab Content 3").tag(3)
+                
+            }
+            .overlay(alignment: .bottom){
+                CustomTabView(tabSelection: $tabSelection)
+            }
         }
     }
 }
